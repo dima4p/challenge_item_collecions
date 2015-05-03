99c0d89bb5fcfebdb5c17bf49f31ebd7fad25995
Dmitri Koulikoff <dima@koulikoff.ru>
2015-05-03 16:55:58 +0300

    Annotation

--- app/models/link_item.rb
+++ app/models/link_item.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: media_items
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  type       :string
+#  user_id    :integer
+#  link       :string
+#  image      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 # Model MediaItem defines the Itmes of kink link for the collecions
 #
 class LinkItem < MediaItem
--- spec/factories/media_items.rb
+++ spec/factories/media_items.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: media_items
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  type       :string
+#  user_id    :integer
+#  link       :string
+#  image      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryGirl.define do
   factory :media_item do
     sequence(:name) {|n| "MediaItem ##{n}" }
