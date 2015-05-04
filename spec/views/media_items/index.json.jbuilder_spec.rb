2be05706ea1bb87474fccb2c1279bebbdfdc6f34
Dmitri Koulikoff <dima@koulikoff.ru>
2015-05-04 11:06:09 +0300

    NAC: hide media data -

--- spec/views/media_items/index.json.jbuilder_spec.rb
+++ spec/views/media_items/index.json.jbuilder_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 describe "media_items/index.json.jbuilder", type: :view do
+  let(:media_item) {create :media_item}
   before(:each) do
     allow(controller).to receive(:can?).and_return(true)
-    @media_item = create(:media_item)
-    assign :media_items, [@media_item, @media_item]
+    assign :media_items, [media_item, media_item]
   end
 
   attributes = %w[
@@ -12,27 +12,57 @@ describe "media_items/index.json.jbuilder", type: :view do
     name
     type
     user_id
-    link
-    image
     url
   ]
+  link_attributes = attributes + ['link']
+  photo_attributes = attributes + ['image']
+  video_attributes = attributes + ['link']
 
-  it "renders a list of media_items as json with following attributes: #{attributes.join(', ')}" do
-    render
+  describe 'for LinkItem' do
+    let(:media_item) {create :link_item}
 
-    hash = MultiJson.load rendered
-    expect(hash.first).to eq(hash = hash.last)
-    expect(hash.keys.sort).to eq attributes.sort
-    expected = @media_item.attributes.slice *attributes
-    expected = MultiJson.load MultiJson.dump expected
-    expected['url'] = media_item_url(@media_item, format: 'json')
-    expect(hash).to eq expected
-    # expect(hash['id']).to eq @media_item.id.to_s
-    # expect(hash['name']).to eq @media_item.name.to_s
-    # expect(hash['type']).to eq @media_item.type.to_s
-    # expect(hash['user']).to eq @media_item.user.to_s
-    # expect(hash['link']).to eq @media_item.link.to_s
-    # expect(hash['image']).to eq @media_item.image.to_s
-    # expect(hash['url']).to eq media_item_url(@media_item, format: 'json')
+    it "renders a list of media_items as json with following attributes: #{link_attributes.join(', ')}" do
+      render
+
+      hash = MultiJson.load rendered
+      expect(hash.first).to eq(hash = hash.last)
+      expect(hash.keys.sort).to eq attributes.sort
+      expected = media_item.attributes.slice *attributes
+      expected = MultiJson.load MultiJson.dump expected
+      expected['url'] = media_item_url(media_item, format: 'json')
+      expect(hash).to eq expected
+    end
+  end
+
+  describe 'for PhotoItem' do
+    let(:media_item) {create :link_item}
+
+    it "renders a list of media_items as json with following attributes: #{photo_attributes.join(', ')}" do
+      render
+
+      hash = MultiJson.load rendered
+      expect(hash.first).to eq(hash = hash.last)
+      expect(hash.keys.sort).to eq attributes.sort
+      expected = media_item.attributes.slice *attributes
+      expected = MultiJson.load MultiJson.dump expected
+      expected['url'] = media_item_url(media_item, format: 'json')
+      expect(hash).to eq expected
+    end
+  end
+
+  describe 'for VideoItem' do
+    let(:video_item) {create :link_item}
+
+    it "renders a list of media_items as json with following attributes: #{link_attributes.join(', ')}" do
+      render
+
+      hash = MultiJson.load rendered
+      expect(hash.first).to eq(hash = hash.last)
+      expect(hash.keys.sort).to eq attributes.sort
+      expected = media_item.attributes.slice *attributes
+      expected = MultiJson.load MultiJson.dump expected
+      expected['url'] = media_item_url(media_item, format: 'json')
+      expect(hash).to eq expected
+    end
   end
 end
