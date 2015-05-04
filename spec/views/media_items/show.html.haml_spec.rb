01d691b4298e2e4907eb10ee49ec75a1a8c353db
Dmitri Koulikoff <dima@koulikoff.ru>
2015-05-04 11:22:59 +0300

    NAC: hide media data -

--- spec/views/media_items/show.html.haml_spec.rb
+++ spec/views/media_items/show.html.haml_spec.rb
@@ -1,17 +1,48 @@
 require 'rails_helper'
 
 describe "media_items/show", type: :view do
+  let (:media_item) {create :media_item}
   before(:each) do
     allow(controller).to receive(:can?).and_return(true)
-    @media_item = assign(:media_item, create(:media_item))
+    assign(:media_item, media_item)
   end
 
-  it "renders attributes in <dl>" do
-    render
-    assert_select 'dl>dd', text: Regexp.new(@media_item.name.to_s)
-    assert_select 'dl>dd', text: Regexp.new(@media_item.type.to_s)
-    assert_select 'dl>dd', text: Regexp.new(@media_item.user.name)
-    assert_select 'dl>dd', text: Regexp.new(@media_item.link.to_s)
-    assert_select 'dl>dd', text: Regexp.new(@media_item.image.to_s)
+  describe 'for LinkItem' do
+    let(:media_item) {create :link_item, image: 'image'}
+
+    it "renders attributes in <dl> without image" do
+      render
+      assert_select 'dl>dd', text: Regexp.new(media_item.name.to_s)
+      assert_select 'dl>dd', text: Regexp.new(media_item.type.to_s)
+      assert_select 'dl>dd', text: Regexp.new(media_item.user.name)
+      assert_select 'dl>dd', text: Regexp.new(media_item.link.to_s)
+      assert_select 'dl>dd', text: Regexp.new(media_item.image.to_s), count: 0
+    end
+  end
+
+
+  describe 'for PhotoItem' do
+    let(:media_item) {create :photo_item, link: 'link'}
+
+    it "renders attributes in <dl> without image" do
+      render
+      assert_select 'dl>dd', text: Regexp.new(media_item.name.to_s)
+      assert_select 'dl>dd', text: Regexp.new(media_item.type.to_s)
+      assert_select 'dl>dd', text: Regexp.new(media_item.user.name)
+      assert_select 'dl>dd', text: Regexp.new(media_item.link.to_s), count: 0
+      assert_select 'dl>dd', text: Regexp.new(media_item.image.to_s)
+    end
+  end
+  describe 'for VideoItem' do
+    let(:media_item) {create :video_item, image: 'image'}
+
+    it "renders attributes in <dl> without image" do
+      render
+      assert_select 'dl>dd', text: Regexp.new(media_item.name.to_s)
+      assert_select 'dl>dd', text: Regexp.new(media_item.type.to_s)
+      assert_select 'dl>dd', text: Regexp.new(media_item.user.name)
+      assert_select 'dl>dd', text: Regexp.new(media_item.link.to_s)
+      assert_select 'dl>dd', text: Regexp.new(media_item.image.to_s), count: 0
+    end
   end
 end
