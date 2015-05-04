d80111c67fc931b0e617b86575953289a30cb4d6
Dmitri Koulikoff <dima@koulikoff.ru>
2015-05-04 10:00:53 +0300

    NAC: it {should validate_uniqueness_of(:name).scoped_to(:user_id) fixed

--- app/models/media_item.rb
+++ app/models/media_item.rb
@@ -19,6 +19,7 @@ class MediaItem < ActiveRecord::Base
   belongs_to :user
 
   validates :name, presence: true
+  validates_uniqueness_of :name, scope: :user_id
 
   scope :ordered, -> { order(:name) }
 
